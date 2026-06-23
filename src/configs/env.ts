766ecc1bc627748d4c6b86241684e8c89c0fe4e2
BarrenTiger <1518353168@qq.com>
2026-06-23 22:30:40 +0800

    替换为幸运转盘网页，清空旧博客

--- src/configs/env.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  env: 'laoluzhikao-3g20kagx4e750bb5'
-}
--- src/configs/routers.ts
+++
@@ -1,9 +0,0 @@
-import INDEX from '../pages/index.jsx';
-import SETTINGS from '../pages/settings.jsx';
-export const routers = [{
-                id: "index",
-                component: INDEX
-              }, {
-                id: "settings",
-                component: SETTINGS
-              }]
